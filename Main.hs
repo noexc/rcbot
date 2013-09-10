551211d7ed143b09f60eae76330f0a957a8cd078
Ricky Elrod <ricky@elrod.me>
2013-09-10 18:42:00 -0400

    First commit.

---
+++ Main.hs
@@ -0,0 +1,71 @@
+module Main where
+
+import Control.Concurrent
+import Control.Monad
+import Network
+import qualified Network.Socket as S
+import Network.IRC
+import System.IO
+import Text.Printf
+
+server = "irc.freenode.net"
+port = 6667
+nickname = "w8upd-rc"
+channel = "#qsolog"
+
+bot :: String -> IO Handle
+bot nickname = withSocketsDo $ do
+  h <- connectTo server (PortNumber (fromIntegral port))
+  hSetBuffering h NoBuffering
+  write h "NICK" nickname
+  write h "USER" (nickname ++ " 0 * :Recent Changes Bot")
+  --mapM (write h "JOIN") channels
+  write h "JOIN" channel
+  return h
+
+write :: Handle -> String -> String -> IO ()
+write h s t = do
+  hPrintf h "%s %s\r\n" s t
+  printf    "> %s %s\n" s t
+
+botListen :: Handle -> IO ()
+botListen h = forever $ do
+    s <- hGetLine h
+    handleLine h $ decode s
+    putStrLn s
+  where
+    forever a = a >> forever a
+
+handleLine :: Handle -> Maybe Message -> IO ()
+handleLine h Nothing = return ()
+handleLine h (Just m) = reply $ msg_command m
+  where
+    params = msg_params m
+
+    reply "PRIVMSG" =
+      case response of
+        Just x -> write h "PRIVMSG" $ channel ++ " :" ++ x
+        Nothing -> return ()
+    reply "PING" = write h "PONG " $ ":" ++ params !! 0
+    reply _ = return ()
+
+    response =
+      case (msg_params m !! 1) of
+        "!hello" -> Just "hey there"
+        _ -> Nothing
+
+udpServer :: Handle -> IO ()
+udpServer h = withSocketsDo $ do
+    sock <- S.socket S.AF_INET S.Datagram 0
+    S.bindSocket sock (S.SockAddrInet 2000 S.iNADDR_ANY)
+    forever $ do
+      (mesg, recv_count, client) <- S.recvFrom sock 1024
+      send_count <- S.sendTo sock mesg client
+      write h "PRIVMSG" $ channel ++ " :" ++ mesg
+      putStrLn mesg
+
+main = do
+  h <- bot nickname
+  forkIO $ do
+    botListen h
+  udpServer h
---
+++ README.md
@@ -0,0 +1,9 @@
+# rcbot
+
+This is our mediawiki recent changes bot, written in Haskell.
+
+It is very experimental and fairly ugly, but it does work.
+
+# License
+
+BSD3, although I'd suggest not use it for anything important.
